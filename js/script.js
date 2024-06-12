0a6c403d3eeebe94b1a7b1f37687f4af13c54e51
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-06-12 07:57:44 +0200

    .

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ document.addEventListener('click', (e) => {
     }
 });
 
-// stop transition and animatino during window resizing
+// Stop transition and animatino during window resizing
 let resizeTimer;
 window.addEventListener('resize', () => {
     document.body.classList.add("resize-animation-stopper");
